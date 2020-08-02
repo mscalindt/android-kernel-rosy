c82e92838b0ccea6ec55189260aa7fe9c193b329
Dimitar Yurukov <mscalindt@protonmail.com>
2020-08-02 17:35:06 +0300

    drivers: rosy: goodix: Fix conditional retardness
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- drivers/rosy/fingerprint/goodix/gf_spi.c
+++ drivers/rosy/fingerprint/goodix/gf_spi.c
@@ -561,25 +561,23 @@ static int gf_open(struct inode *inode, struct file *filp)
 		}
 	}
 
-	if (status == 0) {
-		if (status == 0) {
-			gf_dev->users++;
-			filp->private_data = gf_dev;
-			nonseekable_open(inode, filp);
-			pr_info("Succeed to open device. irq = %d\n",
-					gf_dev->irq);
-			if (gf_dev->users == 1) {
-				status = gf_parse_dts(gf_dev);
-				if (status)
-					goto err_parse_dt;
+	if (!status) {
+		gf_dev->users++;
+		filp->private_data = gf_dev;
+		nonseekable_open(inode, filp);
+		pr_info("Succeed to open device. irq = %d\n",
+				gf_dev->irq);
+		if (gf_dev->users == 1) {
+			status = gf_parse_dts(gf_dev);
+			if (status)
+				goto err_parse_dt;
 
-				status = irq_setup(gf_dev);
-				if (status)
-					goto err_irq;
-			}
-			gf_hw_reset(gf_dev, 3);
-			gf_dev->device_available = 1;
+			status = irq_setup(gf_dev);
+			if (status)
+				goto err_irq;
 		}
+		gf_hw_reset(gf_dev, 3);
+		gf_dev->device_available = 1;
 	} else {
 		pr_info("No device for minor %d\n", iminor(inode));
 	}
